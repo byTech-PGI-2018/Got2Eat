69ee7202ae23cda2bade7589cd51c315beda259c
Joao Montenegro <jpmontenegro67@gmail.com>
2018-12-13 12:28:53 +0000

    Saves messages

---
+++ app/src/main/java/bytech/got2eat/History.java
@@ -0,0 +1,12 @@
+package bytech.got2eat;
+
+import java.util.ArrayList;
+
+public class History {
+
+   public ArrayList<Message> history = null;
+
+   public History(ArrayList<Message> history){
+       this.history = history;
+   }
+}
