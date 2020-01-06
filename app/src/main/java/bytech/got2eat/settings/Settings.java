bd059a3d7f23e9da55135954299e0ccc8484d7a8
filipao223 <jpmontenegro67@gmail.com>
2020-01-06 22:23:38 +0000

    Settings page.

--- app/src/main/java/bytech/got2eat/settings/Settings.java
+++ app/src/main/java/bytech/got2eat/settings/Settings.java
@@ -1,7 +1,8 @@
 package bytech.got2eat.settings;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 import bytech.got2eat.R;
 
@@ -9,9 +10,27 @@ public class Settings extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*Customize toolbar options (toolbar appears due to theme in manifest)*/
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle(getString(R.string.settings));
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        /*Add the fragment containing the PreferenceScreen*/
         getSupportFragmentManager()
         .beginTransaction()
         .replace(android.R.id.content, new SettingsFragment())
         .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*If we click on the '<-' arrow, finish the activity*/
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/bytech/got2eat/settings/SettingsFragment.java
+++ app/src/main/java/bytech/got2eat/settings/SettingsFragment.java
@@ -1,12 +1,31 @@
 package bytech.got2eat.settings;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import bytech.got2eat.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        /*Select our preference screen XML*/
         setPreferencesFromResource(R.xml.settings_prefs, rootKey);
+
+        /*Open a web browser activity when clicking on 'feedback' preference*/
+        Preference github = findPreference("github");
+        if (github != null){
+            github.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/byTech-PGI-2018/Got2Eat"));
+                    startActivity(browserIntent);
+
+                    return true;
+                }
+            });
+        }
     }
 }
